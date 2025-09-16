349369fc0aea5f648f4686d980aeec46af06408c
Shivam Jha <heyshiv2002@gmail.com>
2025-09-16 16:33:44 +0000

    Ok revoke it. I din't liked that

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -23,10 +23,7 @@ function FunZonePageContent() {
 
     return (
         <div 
-            className="flex flex-col h-screen bg-cover bg-center"
-            style={{
-                backgroundImage: "url('https://images.unsplash.com/photo-1542296332-9a5423c21468?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
-            }}
+            className="flex flex-col h-screen"
         >
             <PageHeader 
                 searchTerm=""
