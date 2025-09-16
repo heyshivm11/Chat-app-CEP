f945b22cc97366b7bd1133cc3a7dd994eeaf37a5
Shivam Jha <heyshiv2002@gmail.com>
2025-09-16 23:56:53 +0000

    I don't know why you are not understanding that the copy cust 2 button i

--- src/components/customer-details-card.tsx
+++ src/components/customer-details-card.tsx
@@ -340,7 +340,7 @@ function CustomerDetailsCardComponent({
         description: "Have you copied the customer details? They might be important for your records.",
         duration: 8000,
         action: (
-          <div className="flex flex-col sm:flex-row gap-2 mt-2 sm:mt-0 w-full">
+          <div className="flex flex-col gap-2 mt-2 w-full">
             <ToastAction altText="Copy details for Customer 1" onClick={() => copyDetails(1)}>
                 Copy Cust. 1
             </ToastAction>
