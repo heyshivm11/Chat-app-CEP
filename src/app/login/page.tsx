59bbe201753af549ade551b858be146a34ea6c0e
Shivam Jha <heyshiv2002@gmail.com>
2025-09-16 16:29:02 +0000

    Relace with ths image

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
     <div 
       className="flex items-center justify-center min-h-screen bg-cover bg-center p-4"
       style={{
-        backgroundImage: "url('https://images.unsplash.com/photo-1516979187457-637abb4f9353?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+        backgroundImage: "url('https://images.unsplash.com/photo-1436491865332-7a61a109cc05?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
       }}
     >
       <Card className="w-full max-w-sm bg-background/80 backdrop-blur-sm">
